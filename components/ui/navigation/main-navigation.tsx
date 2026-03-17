2f34acbd67b27ef4c7f9a6e99859ab22d1a4ea3b
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-17 16:08:37 +0100

    Add green dot to image, responsivity to navbar, hover elements and links
    Change page.tsx to be empty and only contain <main> with main navigation inside

--- components/ui/navigation/main-navigation.tsx
+++ components/ui/navigation/main-navigation.tsx
@@ -2,15 +2,24 @@ import Link from "next/link";
 
 export default function MainNavigation() {
   return (
-    <>
-      <nav className="flex justify-between py-2 px-4 bg-gray-700">
-        <div>
-          <img className="rounded-4xl mb-2" src="https://i.pravatar.cc/40" alt="" />
-          <span>Exempel Användare</span>
-        </div>
-        <span>Job Tracker</span>
-        <Link href={"#"}>Min sida</Link>
-      </nav>
-    </>
+    <nav className="flex justify-between items-center py-4 px-2 sm:px-4 bg-gray-700">
+      <div className="relative w-10 h-10">
+        <img
+          className="rounded-full curosr-pointer"
+          src="https://i.pravatar.cc/40"
+          alt=""
+        />
+        <span className="absolute top-0 right-0 bg-green-400 size-2.5 rounded-full border border-gray-700"></span>
+      </div>
+      <Link
+        className="sm:text-3xl md:text-4xl text-2xl hover:opacity-80"
+        href={"#"}
+      >
+        Job Tracker
+      </Link>
+      <Link className="hover:opacity-80" href={"#"}>
+        Min sida
+      </Link>
+    </nav>
   );
 }
