3fe0217eb3edc4d70a588c6bc2f65976b2c272cd
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-17 16:58:45 +0100

    Add some functions regarding crud to db.ts, no functionality yet

---
+++ lib/db.ts
@@ -0,0 +1,22 @@
+const API_URL = "http://localhost:4000/"
+
+// Should recieve a response from our api, then change it to a json, then return it 
+export async function  getAllPosts() {
+    const response = await fetch(`${API_URL}`) // Get
+}
+
+export async function getPostById(id:number) {
+
+}
+
+export async function deletePostById (id:number) {
+
+}
+
+export async function updatePostById (id:number) {
+
+}
+
+export async function createPost () {
+
+}
