6edf20022cea7aacc8b28473f3a5015af62a0f94
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-17 16:08:37 +0100

    Add green dot to image, responsivity to navbar, hover elements and links
    Change page.tsx to be empty and only contain <main> with main navigation inside

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import JobPostCardGrid from "@/components/ui/job-post-grid";
+import MainNavigation from "@/components/ui/navigation/main-navigation";
 
 export default function Home() {
   return (
     <main>
+      <MainNavigation />
       <JobPostCardGrid></JobPostCardGrid>
     </main>
   );
