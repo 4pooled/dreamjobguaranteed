c65f5f05bf988142e26f48aa1c44a7a8e5085af2
Slackz0r <gustav.aldenberg@gmail.com>
2026-03-18 13:26:17 +0100

    add search bar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import JobPostCardGrid from "@/components/ui/job-post-grid";
 import MainNavigation from "@/components/ui/navigation/main-navigation";
+import SearchBar from "@/components/ui/search-bar";
 
 export default function Home() {
   return (
     <main>
       <MainNavigation />
+      <SearchBar />
       <JobPostCardGrid></JobPostCardGrid>
     </main>
   );
---
+++ components/ui/search-bar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
+export default function SearchBar() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const setParam = (key: string, value: string) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (value) {
+      params.set(key, value);
+    } else {
+      params.delete(key);
+    }
+
+    replace(`${pathname}?${params.toString()}`);
+  };
+
+  return (
+    <section className="w-full">
+      <div>
+        <label htmlFor="search-input">Search:</label>
+
+        <input
+          type="text"
+          id="search-input"
+          placeholder="Search posts..."
+          className="rounded-xl border border-gray-300 bg-gray-50 px-4 py-2 text-sm text-gray-900 placeholder:text-gray-400 outline-none transition focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200"
+          onChange={(e) => {
+            setParam("q", e.target.value);
+          }}
+        />
+      </div>
+    </section>
+  );
+}
