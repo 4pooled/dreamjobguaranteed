a4ac8b0439811e0ebd199208ed97370462230e59
Alex <olofalexander.larsson@gmail.com>
2026-03-18 16:12:26 +0100

    Added component to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import JobPostCardGrid from "@/components/ui/job-post-grid";
 import MainNavigation from "@/components/ui/navigation/main-navigation";
+import CreatePostModal from "@/components/ui/modals/create-jobpost-form";
 
 export default function Home() {
   return (
     <main>
       <MainNavigation />
       <JobPostCardGrid></JobPostCardGrid>
+      <CreatePostModal />
     </main>
   );
 }
