d357c6a760f1c9b210ab26543c9de3a1f720a77c
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-18 13:55:33 +0100

    Add initial updatePost function with props, needs functionality

--- lib/actions.ts
+++ lib/actions.ts
@@ -24,3 +24,21 @@ export async function createPost(formData: {
   file.jobs.push(newPost);
   fs.writeFileSync(filePath, JSON.stringify(file, null, 2));
 }
+
+export async function updatePost(id: number, formData: {
+  title: string;
+  description: string;
+  category: string;
+  hyperlink: string;
+}) {
+ // denna funktion måste skapas med för att kunna uppdatera poster, så som du gjort ovan
+ 
+  const filePath = path.join(process.cwd(), "server", "db.json");
+  const file = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+  // 1. hitta index för post som ska uppdateras
+  
+
+
+  // 2. uppdatera objektet, men behåll gamla värden som inte ska uppdateras
+}
