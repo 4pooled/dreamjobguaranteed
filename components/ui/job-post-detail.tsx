7aa4b23ac523365686fe067fde8fe134b25fa73c
4pooled <edchallis@protonmail.com>
2026-03-18 10:34:21 +0100

    added postData and basic styling for job-post-detail.tsx

--- components/ui/job-post-detail.tsx
+++ components/ui/job-post-detail.tsx
@@ -1,11 +1,29 @@
 import { JobPost } from "@/lib/types";
+import Link from "next/link";
 
 export default function JobPostDetail ({ postData }: { postData: JobPost}) {
+  if (!Object.keys(postData).length) {
+    return (
+      <h1>Post was not found.</h1>
+    );
+  }
+
   return (
-    <div className="flex flex-col gap-4">
-      <h1>{postData.title}</h1>
-      <p>{postData.hyperlink}</p>
-      <p>{postData.companyHyperlink}</p>
+    <div className="m-5">
+      <Link
+	href="/"
+	className="border p-1 rounded-lg"
+      >
+	Go Back
+      </Link>
+      <div className="flex flex-col gap-2 mt-5 w-200 mx-auto">
+	<h1 className="text-center text-xl font-bold">{postData.title}</h1>
+	<p className="text-center">{postData.category}</p>
+	<p className="text-pretty">{postData.description}</p>
+	<br />
+	<p className="">{postData.hyperlink}</p>
+	<p className="">{postData.companyHyperlink}</p>
+      </div>
     </div>
   );
 }
