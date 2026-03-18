92aed680213e531d9f16dbdfae34d1de69e1261c
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-18 08:26:21 +0100

    Add successful fetch of all posts

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,10 @@ const API_URL = "http://localhost:4000/"
 
 // Should recieve a response from our api, then change it to a json, then return it 
 export async function  getAllPosts() {
-    const response = await fetch(`${API_URL}`) // Get
+    const response = await fetch(`${API_URL}jobs`) // Get
+    const data = await response.json()
+    return data
+    
 }
 
 export async function getPostById(id:number) {
