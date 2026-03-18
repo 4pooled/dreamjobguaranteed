b87170fc1f5ae55bc21781f0d20912b871dbecb3
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-18 10:30:19 +0100

    Add get post by id and delete post by id

--- lib/db.ts
+++ lib/db.ts
@@ -1,25 +1,37 @@
+import { revalidatePath } from "next/cache"
+
 const API_URL = "http://localhost:4000/"
 
 // Should recieve a response from our api, then change it to a json, then return it 
 export async function  getAllPosts() {
     const response = await fetch(`${API_URL}jobs`) // Get
-    const data = await response.json()
+    const data = await response.json() // Convert
     return data
     
 }
 
 export async function getPostById(id:number) {
-
+    const response = await fetch(`${API_URL}jobs/${id}`)
+    const data = await response.json()
+    return data
 }
 
 export async function deletePostById (id:number) {
+    // method: DELETE?
+    const response = await fetch(`${API_URL}jobs/${id}`, {
+        method: "DELETE"
+
+    })
+if (!response.ok) return false
+
+revalidatePath("")
+return true
+}
+
+export async function updatePostById (id:number, data:string) {
 
 }
 
-export async function updatePostById (id:number) {
-
-}
-
-export async function createPost () {
+export async function createPost (data:string) {
 
 }
