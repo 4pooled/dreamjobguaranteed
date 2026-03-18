7bdc985c7e0ec244f12c93f182d9bf27bea35c2c
4pooled <edchallis@protonmail.com>
2026-03-18 10:44:02 +0100

    prettier things

--- app/job-post/[id]/page.tsx
+++ app/job-post/[id]/page.tsx
@@ -3,13 +3,13 @@ import JobPostDetail from "@/components/ui/job-post-detail";
 export default async function JobPost({ params }: PageProps<"/job-post/[id]">) {
   const { id } = await params;
 
-  const postData = await fetch(
-    `http://localhost:4000/jobs/${id}`
-  ).then(res => res.json());
+  const postData = await fetch(`http://localhost:4000/jobs/${id}`).then((res) =>
+    res.json(),
+  );
 
   return (
     <div>
-       <JobPostDetail postData={postData} />
+      <JobPostDetail postData={postData} />
     </div>
   );
 }
--- components/ui/job-post-detail.tsx
+++ components/ui/job-post-detail.tsx
@@ -1,28 +1,23 @@
 import { JobPost } from "@/lib/types";
 import Link from "next/link";
 
-export default function JobPostDetail ({ postData }: { postData: JobPost}) {
+export default function JobPostDetail({ postData }: { postData: JobPost }) {
   if (!Object.keys(postData).length) {
-    return (
-      <h1>Post was not found.</h1>
-    );
+    return <h1>Post was not found.</h1>;
   }
 
   return (
     <div className="m-5">
-      <Link
-	href="/"
-	className="border p-1 rounded-lg"
-      >
-	Go Back
+      <Link href="/" className="border p-1 rounded-lg">
+        Go Back
       </Link>
       <div className="flex flex-col gap-2 mt-5 w-200 mx-auto">
-	<h1 className="text-center text-xl font-bold">{postData.title}</h1>
-	<p className="text-center">{postData.category}</p>
-	<p className="text-pretty">{postData.description}</p>
-	<br />
-	<p className="">{postData.hyperlink}</p>
-	<p className="">{postData.companyHyperlink}</p>
+        <h1 className="text-center text-xl font-bold">{postData.title}</h1>
+        <p className="text-center">{postData.category}</p>
+        <p className="text-pretty">{postData.description}</p>
+        <br />
+        <p className="cursor-pointer">{postData.hyperlink}</p>
+        <p className="cursor-pointer">{postData.companyHyperlink}</p>
       </div>
     </div>
   );
