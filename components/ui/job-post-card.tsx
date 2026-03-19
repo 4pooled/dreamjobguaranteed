78b03784ef8f487d85866263a3633dc0307aaef9
victor <victnek@gmail.com>
2026-03-19 10:34:03 +0100

    Add gr8 new job

--- components/ui/job-post-card.tsx
+++ components/ui/job-post-card.tsx
@@ -5,9 +5,9 @@ export default function JobPostCard({ jobPost }: { jobPost: JobPost }) {
   return (
     <Link
       href={`/job-post/${jobPost.id}`}
-      className={`block relative border rounded-xl shadow-md overflow-hidden hover:animate-spin-once`}
+      className={`h-full bg-gray-500 block relative border rounded-xl shadow-md overflow-hidden hover:animate-spin-once`}
     >
-      <div className="p-2 bg-black/40 text-white overflow-hidden">
+      <div className="p-2 text-white overflow-hidden">
         <h3 className="text-2xl font-bold text-center">{jobPost.title}</h3>
         <p>{jobPost.description}</p>
       </div>
